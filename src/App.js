deabe3534e50e6e29f47ec4d99ccf213d4b8d52d
João Victor <reflixplayshd@gmail.com>
2022-03-13 01:19:00 -0300

    Style finished

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import './App.css';
+import logo from './assets/image.svg';
 
 function App() {
   return (
     <div className="App">
-      <div className="logo">
-        <span>Logo Empresa</span>
-      </div>
+      <header className="header">
+        <p>Logo Empresa</p>
 
-      <div className="nav">
-        <ul>
+        <ul className="nav">
           <li>Início</li>
           <li>Serviços</li>
           <li>Sobre</li>
           <li>Contato</li>
           <li>Já tem uma conta?</li>
         </ul>
-      </div>
+      </header>
 
       <main className="main">
-        <h1>Inscreva-se agora na Newsletter</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+        <div>
+          <div className="newsletterContainer">
+            <h1>
+              <span>Inscreva-se agora</span> na Newsletter
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
 
-        <div className="input">
-          <input type="text"></input>
-          <input type="email"></input>
-          <button onClick="()=>{}">Enviar</button>
+          <div className="input">
+            <input type="text" placeholder="Nome"></input>
+            <input type="email" placeholder="Email"></input>
+            <button onClick={() => {}}>Enviar</button>
+          </div>
+        </div>
+
+        <div>
+          <img src={logo} alt="girl"></img>
         </div>
       </main>
     </div>
